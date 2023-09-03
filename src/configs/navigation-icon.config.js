62f51a770c39735ce5b79c5e7e8c5e99fd215485
marko-lazarevic <marko.lazarevic@redbudi.com>
2023-09-03 13:01:45 +0200

    Added icons

--- src/configs/navigation-icon.config.js
+++ src/configs/navigation-icon.config.js
@@ -6,6 +6,13 @@ import {
     HiOutlineViewGridAdd,
     HiOutlineHome,
 } from 'react-icons/hi'
+import { TbCategory2, TbFlag2 } from 'react-icons/tb'
+import { BiSolidCity } from 'react-icons/bi'
+import { BsBuildingGear, BsGear, BsImage } from 'react-icons/bs'
+import { LiaCitySolid } from 'react-icons/lia'
+import { MdOutlineBrandingWatermark, MdOutlineWarehouse } from 'react-icons/md'
+import { LuNewspaper } from 'react-icons/lu'
+import { PiShoppingBag } from 'react-icons/pi'
 
 const navigationIcon = {
     home: <HiOutlineHome />,
@@ -13,6 +20,16 @@ const navigationIcon = {
     collapseMenu: <HiOutlineTemplate />,
     groupSingleMenu: <HiOutlineDesktopComputer />,
     groupCollapseMenu: <HiOutlineColorSwatch />,
+    category: <TbCategory2 />,
+    flag: <TbFlag2 />,
+    city: <LiaCitySolid />,
+    manufacturer: <BsBuildingGear />,
+    brand: <MdOutlineBrandingWatermark />,
+    warehouse: <MdOutlineWarehouse />,
+    newsletter: <LuNewspaper />,
+    banner: <BsImage />,
+    gear: <BsGear />,
+    product: <PiShoppingBag />,
 }
 
 export default navigationIcon
